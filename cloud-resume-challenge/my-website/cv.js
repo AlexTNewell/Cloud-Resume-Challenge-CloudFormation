9871ea2e489cb933fe7b5ba1a3ff2ef20626647f
Alexander Newell <122984252+AlexanderNewell@users.noreply.github.com>
2023-05-08 15:52:44 +0100

    Delete cloud-resume-challenge directory

--- cloud-resume-challenge/my-website/cv.js
+++
@@ -1,5 +0,0 @@
-fetch('https://hr1w97khtd.execute-api.us-east-1.amazonaws.com/Prod/get')
-    .then(response => response.json())
-    .then((data) => {
-        document.getElementById('replaceme').innerText = data.count
-    })
