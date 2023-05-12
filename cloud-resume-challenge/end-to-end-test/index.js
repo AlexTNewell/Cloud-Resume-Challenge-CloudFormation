4b3575fcb25e46a7bd43f91d0fbcfd6e9dc1773c
Alexander Newell <122984252+AlexanderNewell@users.noreply.github.com>
2023-05-12 11:40:35 +0100

    Update index.js

--- cloud-resume-challenge/end-to-end-test/index.js
+++ cloud-resume-challenge/end-to-end-test/index.js
@@ -12,17 +12,3 @@ async function foo() {
 }
 
 return foo();
-
-
-
-async function twoHundred() { 
-    let status;
-
-    const response = await fetch('https://hr1w97khtd.execute-api.us-east-1.amazonaws.com/Prod/count').then((response) => {expect(response).to.have.property('status', 200)
-
-    answer = await response.json();
-
-    console.log(answer);
-}
-
-return twoHundred();
