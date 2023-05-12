3cd3cd576b106035578b593f9b642842ea9eceb2
Alexander Newell <122984252+AlexanderNewell@users.noreply.github.com>
2023-05-12 11:18:48 +0100

    Update index.js

--- cloud-resume-challenge/end-to-end-test/index.js
+++ cloud-resume-challenge/end-to-end-test/index.js
@@ -1,12 +1,14 @@
 const fetch = require("node-fetch");
 
 async function foo() {
-  let obj;
+    let obj;
 
-  const res = await fetch('https://hr1w97khtd.execute-api.us-east-1.amazonaws.com/Prod/count')
+    const res = await fetch('https://hr1w97khtd.execute-api.us-east-1.amazonaws.com/Prod/count')
 
-  obj = await res.json();
-  
-  }
-  
-return foo()
+    obj = await res.json();
+
+    console.log(obj);
+
+}
+
+return foo();
